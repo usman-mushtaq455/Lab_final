bc049266fdd59f9f99c6b161ca64f902bf4fa3e1
usman <usman@LAPTOP-FSVI2UHO>
2020-01-03 16:06:29 +0500

    Java doc created

--- calculator/src/main/java/calculator/Calculator.java
+++ calculator/src/main/java/calculator/Calculator.java
@@ -1,7 +1,14 @@
 package calculator;
 
 public class Calculator {
-	
+	/***
+	 * This method finds the maximum in three integers
+	 * @param num1 the first integer
+	 * @param num2 the second integer
+	 * @param num3 the third integer
+	 * @return maximum integer
+	 */
+
 public int findMax(int num1, int num2, int num3) {
 		int[] arr = { num1, num2, num3 };
 		int max = 0;
@@ -11,6 +18,14 @@ public int findMax(int num1, int num2, int num3) {
       }
 		return max;
 }
+/**
+ * This method calculates the square of a positive integer, which is 
+   * less than 10 and greater than 0
+ * @param num the input integer
+ * @return the square of the input integer
+ * @throws IllegalArgumentException when the input integer is not 
+   * between <code>0</code> and <code>10</code>
+ */
 
 public int square(int num) throws IllegalArgumentException {
    int result = 0;
@@ -23,10 +38,15 @@ public int square(int num) throws IllegalArgumentException {
    return result;
 }
 
+/**
+ * This method calculates the cube of an integer
+ * @param num input integer
+ * @return the cube of the input integer
+ */
+
 public int cube(int num) {
-	int result=0;
-	result=num*num*num;
-	return result;
+	
+	return num*num*num;
 	
 }
 
